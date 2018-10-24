d4b1c86f26e5fb0e511070cdc88b66689baa8192
Lanyard <40360728+Lanyard31@users.noreply.github.com>
2018-10-24 16:36:31 +0200

    First sprites, prototype begun

--- Root.gd
+++ Root.gd
@@ -1,7 +1,13 @@
 extends Node
 
 #What's up dude
+#I'm not afraid to die
 
 func _ready():
 	pass
-	#I'm not afraid to die
+
+func _on_NoGravitySwimmer_pressed():
+	get_tree().change_scene("res://Level1.tscn")
+
+func _on_Gravity_pressed():
+	get_tree().change_scene("res://Level1.tscn")
---
+++ gallet/cavesofgallet/cavesofgallet_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.1" tiledversion="2018.09.12" name="cavesofgallet_tiles" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="cavesofgallet_tiles.png" width="64" height="96"/>
+</tileset>
